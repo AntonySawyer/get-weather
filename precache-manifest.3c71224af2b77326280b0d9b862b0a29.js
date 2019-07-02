e0e41d076ec6ba5dee31635da4fd65bf5184366f
Antony <A.Krutenok@gmail.com>
2019-07-02 12:25:34 +0300

    Updates

---
+++ precache-manifest.3c71224af2b77326280b0d9b862b0a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a411d915833d3e83f6a75d7993a0d7",
+    "url": "/get-weather/index.html"
+  },
+  {
+    "revision": "60252a4366267693a613",
+    "url": "/get-weather/static/css/main.49498073.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/get-weather/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "60252a4366267693a613",
+    "url": "/get-weather/static/js/main.e1aac31f.chunk.js"
+  },
+  {
+    "revision": "e9e06f0e7e0e0ec89a6c",
+    "url": "/get-weather/static/js/runtime~main.c44707df.js"
+  }
+]);
